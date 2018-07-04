4585ba9f1c19696fbe12add4cd8c88fc36b39a1c
bigzhu <vermiliondun@gmail.com>
2018-07-04 18:27:46 +0800

    指定当前路径

--- confbz/base.go
+++ confbz/base.go
@@ -2,13 +2,15 @@ package confbz
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/BurntSushi/toml"
 )
 
 func getConf(confbzName string, v interface{}) {
-	_, err := toml.DecodeFile(fmt.Sprintf("conf/%s.toml", confbzName), v)
+	_, err := toml.DecodeFile(fmt.Sprintf("./conf/%s.toml", confbzName), v)
 	if err != nil {
+		log.Println(err)
 		_, err := toml.DecodeFile(fmt.Sprintf("../conf/%s.toml", confbzName), v)
 		if err != nil {
 			panic(err)
