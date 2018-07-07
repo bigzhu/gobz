24f0833bcedec66f29ab2cd83c35ec661bc361c0
bigzhu <vermiliondun@gmail.com>
2018-07-07 21:44:05 +0800

    注册成功后返回给客户端

--- websocketbz/register.go
+++ websocketbz/register.go
@@ -13,15 +13,24 @@ type Register struct {
 	Key  string `json:"key"`
 }
 
+// Resp 注册的结果如何
+type Resp struct {
+	Result string
+}
+
 // DoRegister 注册申请的 websocket
 func DoRegister(s *melody.Session, msg []byte) {
 	var register Register
 	err := json.Unmarshal(msg, &register)
 	if err != nil {
 		log.Println(err)
+		result, _ := json.Marshal(Resp{Result: err.Error()})
+		s.Write(result)
 		return
 	}
 	s.Set(register.Key, "bigzhu")
 	log.Println("register new websocket", s.Request.URL.Path, "key=", register.Key)
+	result, _ := json.Marshal(Resp{Result: "success"})
+	s.Write(result)
 	return
 }
