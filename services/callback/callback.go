86941faf4c38d7581038603aeb654f1c5bf90477
bigzhu <vermiliondun@gmail.com>
2018-10-14 18:48:26 +0800

    增加回调

---
+++ services/callback/callback.go
@@ -0,0 +1,57 @@
+// Package callback 针对一些需要 http 反复回调, 并且需要记录回调结果的情况
+package callback
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/bigzhu/gobz/httpbz"
+	"github.com/bigzhu/gobz/modelsbz"
+	"github.com/bigzhu/gobz/telegram"
+	"github.com/jinzhu/gorm"
+	"github.com/pkg/errors"
+)
+
+const (
+	//Success 成功
+	Success = "Success"
+	//Failure callback 失败
+	Failure = "Failure"
+	// NoNeed 不再需要处理, 回调那里
+	NoNeed = "NoNeed"
+)
+
+// Callback 执行回调, 并保存结果. 必须包含以下两个参数
+// callback.URL 地址
+// callback.Request 请求参数
+func Callback(callback CallbackM) {
+	response, statusCode, err := httpbz.Post(callback.URL, callback.Request, nil)
+	if err != nil {
+		callback.ErrorInfo = err.Error()
+		err = nil
+	}
+	callback.StatusCode = statusCode
+	callback.Response = response
+
+	// 如果返回 200 就姑且认为回调成功
+	if callback.StatusCode == http.StatusOK {
+		callback.Status = Success
+	} else {
+		callback.Status = Failure
+	}
+	// 保存
+	callbackData, err := GetCallbackByTypeAndKey(callback.Key, callback.Type)
+	if err == nil {
+		callback.Count = callbackData.Count + 1
+		// 更新错误信息
+		modelsbz.DB.Model(&callbackData).Updates(callback)
+	} else if err == gorm.ErrRecordNotFound {
+		modelsbz.DB.Create(&callback)
+		err = nil
+	} else {
+		err = errors.Wrap(err, "查询回调列表时出错")
+		telegram.Send(err.Error())
+		log.Printf("%+v", err)
+	}
+	return
+}
---
+++ services/callback/models.go
@@ -0,0 +1,33 @@
+package callback
+
+import (
+	"time"
+
+	"github.com/bigzhu/gobz/modelsbz"
+)
+
+// Callback 记录 callback 状态
+type CallbackM struct {
+	ID        int `gorm:"primary_key"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+
+	Request    string
+	Response   string
+	URL        string
+	Count      int    // 调用次数
+	Status     string //Success or Failure or NoNeed
+	StatusCode int
+	ErrorInfo  string
+	Type       string // 考虑兼容多种回调, 根据 Type 做不同操作
+	Key        string `sql:"type:JSONB;"` // 不同Type需要存放一些内容以用于重发取数据, 比如 transferID
+}
+
+// GetCallbackByTypeAndKey 查询回调记录
+func GetCallbackByTypeAndKey(key string, callbackType string) (callback Callback, err error) {
+	err = modelsbz.DB.
+		Where("key = ? and type=?", key, callbackType).
+		First(&callback).
+		Error
+	return
+}
