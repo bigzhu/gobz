0b30e0703e8a7a457cc59f97b6689061a203bf4a
bigzhu <vermiliondun@gmail.com>
2018-11-21 15:25:52 +0800

    建立错误的方法

--- apibz/error.go
+++ apibz/error.go
@@ -5,3 +5,8 @@ type ErrorBz struct {
 	ErrCode string      `json:"err_code"`
 	Payload interface{} `json:"payload"`
 }
+
+// NewErr 新建错误
+func NewErr(code string) *ErrorBz {
+	return &ErrorBz{ErrCode: code}
+}
