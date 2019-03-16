e1edc57ccc8db4e089d88383bbf1d04b3b63c291
bigzhu <vermiliondun@gmail.com>
2019-03-16 16:02:14 +0800

    interface can't be point

--- modelsbz/update_or_create.go
+++ modelsbz/update_or_create.go
@@ -3,7 +3,7 @@ package modelsbz
 import "github.com/jinzhu/gorm"
 
 // UpdateOrCreate if not exist create else update
-func UpdateOrCreate(where interface{}, o *interface{}) (err error) {
+func UpdateOrCreate(where interface{}, o interface{}) (err error) {
 	var i interface{}
 	err = DB.Where(where).First(i).Error
 	// exist update
