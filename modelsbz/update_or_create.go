fc4f6be7207b72c1477f82ea0e928b043918d7fa
bigzhu <vermiliondun@gmail.com>
2019-03-17 23:21:22 +0800

    update or create tell user is exists

--- modelsbz/update_or_create.go
+++ modelsbz/update_or_create.go
@@ -1,10 +1,13 @@
 package modelsbz
 
 // UpdateOrCreate if not exist create else update
-func UpdateOrCreate(where interface{}, o interface{}) (err error) {
+// tell user is exists
+func UpdateOrCreate(where interface{}, o interface{}) (exists bool, err error) {
 	if DB.Model(o).Where(where).Update(o).RowsAffected == 0 {
+		exists = false
 		err = DB.Create(o).Error
 	} else {
+		exists = true
 		err = DB.Model(o).Where(where).First(o).Error
 	}
 	return
