2bda1e5cc28105c11b268796e6c9f9365c87ee53
bigzhu <vermiliondun@gmail.com>
2022-02-24 11:36:12 +0800

    update logic

--- modelsbz/update_or_create.go
+++ modelsbz/update_or_create.go
@@ -1,21 +1,16 @@
 package modelsbz
 
-import (
-	"errors"
-
-	"gorm.io/gorm"
-)
-
 // UpdateOrCreate if not exist create else update
 // tell user is exists
 func UpdateOrCreate(where interface{}, o interface{}) (exists bool, err error) {
-	var result interface{}
-	err = DB.Model(o).Where(where).First(result).Error
+	count := int64(0)
+	err = DB.Model(o).Where(where).Count(&count).Error
 	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			exists = false
-			err = DB.Create(o).Error
-		}
+		return
+	}
+	if count == 0 {
+		exists = false
+		err = DB.Create(o).Error
 	} else {
 		exists = true
 		err = DB.Debug().Model(o).Where(where).Updates(o).Error
